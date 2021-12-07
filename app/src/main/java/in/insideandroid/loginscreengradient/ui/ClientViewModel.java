502f48a7ea876cdf02c833537dd00f48b8a1765d
Lenovo <ghada.touati@outlook.com>
2021-12-07 20:38:44 +0100

    Initial commit

---
+++ app/src/main/java/in/insideandroid/loginscreengradient/ui/ClientViewModel.java
@@ -0,0 +1,7 @@
+package in.insideandroid.loginscreengradient.ui;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ClientViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
